aaa7edd93e5d0a11eb788c94eff87c37cdd862fa
samrustim <a610301@mail.ru>
2024-08-14 02:18:49 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = ("Лондон", "Москва", 15, True,)  # Создайте переменную immutable_var и присвойте ей кортеж из
+# нескольких элементов разных типов данных.
+print(immutable_var)  # Выполните операции вывода кортежа immutable_var на экран
+#  immutable_var[0] = 1
+#  print(immutable_var) #  Нельзя имзенить элемент в кортеже, выдает ошибку
+mutable_list = ["Лондон", "Москва", 15, True,] #  Создайте переменную mutable_list и присвойте ей список из нескольких элементов.
+print(mutable_list)
+mutable_list[0] = "Смоленск"  #  Измените элементы списка mutable_list.
+print(mutable_list) #  Выведите на экран измененный список mutable_list.
